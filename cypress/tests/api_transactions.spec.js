7eb2b3762ce0b9d556beed57406228e0b212bb4a
Aníbal del Real <anibaldelreal@MacBook-Pro-de-Anibal.local>
2024-02-27 18:53:22 -0600

    adding graphql test

--- cypress/tests/api_transactions.spec.js
+++ cypress/tests/api_transactions.spec.js
@@ -5,11 +5,32 @@ describe('API Testing', () => {
         cy.requestApi(logger.username, logger.password);
     });
 
-    it ('Obtener el segundo elemento', () => {
+    it.skip ('Obtener el segundo elemento', () => {
         cy.request("GET", 'http://localhost:3001/transactions')
         .then(response => {
             expect(response.status).to.eq(200);
             expect(response.body.results[0].description).to.equal('Esta es una prueba');
         });
     });
+
+    it('Graphql test', () => {
+        cy.request('POST', 'http://localhost:3001/graphql', {
+            query: `query {
+                listBankAccount {
+                 id
+                 uuid
+                 userId
+                 bankName
+                 accountNumber
+                 routingNumber
+                 isDeleted
+                 createdAt
+                 modifiedAt
+                }
+            }`,
+        }).then((response) => {
+            expect(response.status).to.eq(200);
+            expect(response.body.data.listBankAccount[0].accountNumber).to.equal("3859571950");
+        });
+    })
 });
